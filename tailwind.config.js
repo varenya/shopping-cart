f46c86a107758c7ba1af119293e5378070ce5430
Varenya <varen90@gmail.com>
2021-01-07 21:02:16 +0530

    [VT] Practicing design and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "powder-blue": "#A8DADC",
+        "honey-dew": "#F1FAEE",
+        "imperial-red": "#E63946",
+        "celadon-blue": "#457B9D",
+        "prussian-blue": "#1D3557",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
